bfcd53be3b73e0cb8002909a19b0732cd9f8023c
Chocogrenouille <thestral@gmx.net>
2021-06-23 10:34:57 +0200

    added CRUD part

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -2,8 +2,6 @@
 import { Response, Request } from "express";
 import { ITodo } from "./../../types/todo";
 import ToDo from "../../models/todo";
-import { DESTRUCTION } from "dns";
-import { ifError } from "assert";
 
 // need to define again what the function returns: Promise with type void as it doesn't return a value
 const getToDos = async (req: Request, res: Response): Promise<void> => {
@@ -18,7 +16,8 @@ const getToDos = async (req: Request, res: Response): Promise<void> => {
 
 const addToDo = async (req: Request, res: Response): Promise<void> => {
     try {
-        // ???
+        // asigns req.body to constant and uses typecasting to match ITodo
+        // check out Pick
         const body = req.body as Pick<ITodo, "name" | "description" | "status">
 
         const toDo: ITodo = new ToDo({
@@ -27,10 +26,57 @@ const addToDo = async (req: Request, res: Response): Promise<void> => {
             status: body.status
         });
 
-        const newToDo: ITodo = await ToDo.save();
+        // save() - 
+        // waits for toDo to be created first before it asigns it to newToDo, typescript saves it as an IToDo instance
+        const newToDo: ITodo = await toDo.save();
+        // waits to find all toDos before asigning it to allToDos, typscript saves it as an array of IToDo instances
         const allToDos: ITodo[] = await ToDo.find();
 
-        res.status(201).json({message: "ToDo added", todo:})
+        res.status(201).json({
+            message: "ToDo added", 
+            todo: newToDo, 
+            todos: allToDos
+        });
+
+    } catch (error) {
+        throw error
+    }
+}
+
+const updateToDo = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const {params: {id}, body} = req;
+
+        const updateToDo: ITodo | null = await ToDo.findByIdAndUpdate(
+            {_id: id}, body
+        )
+
+        const allToDos: ITodo[] = await ToDo.find();
+
+        res.status(200).json({
+            message: "ToDo updated",
+            todo: updateToDo,
+            todos: allToDos
+        })
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+const deleteToDo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const deletedToDo: ITodo | null = await ToDo.findByIdAndRemove(
+            req.params.id
+        );
+
+        const allToDos: ITodo[] = await ToDo.find();
+
+        res.status(200).json({
+            message: "Todo deleted",
+            todo: deletedToDo,
+            todos: allToDos
+        })
     } catch (error) {
         throw error
     }
